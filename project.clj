4c2bc6a9bd4c0a769d9b853587a36ae2a65fc85f
Peter Gillard-Moss <pgillard@thoughtworks.com>
2013-07-09 12:28:16 +0000

    Create a main

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject go-data "1.0.0-SNAPSHOT"
   :description "Mine Go for data"
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main go-data.bin
+  :aot [go-data.bin])
---
+++ src/go_data/bin.clj
@@ -0,0 +1,5 @@
+(ns go-data.bin
+  (:gen-class))
+
+(defn -main [& args]
+  (println "Welcome" args))
